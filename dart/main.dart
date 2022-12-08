53c9555950ccc00465ec0539a84a10963303515f
Ajibade Faruk <101510395+1FarukDev@users.noreply.github.com>
2022-12-08 13:19:20 +0100

    Add files via upload

---
+++ dart/main.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = "Faruk";
+  String surname = "Ajibade";
+  int age = 50;
+  // bool lovesbeans = true;
+  print("My name is $name $surname $age i hope you enjoyed it");
+}
